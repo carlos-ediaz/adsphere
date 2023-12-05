e26b251bd5135d322c592db655a53e5f78e7f4c7
Carlos Diaz <carlos9559@gmail.com>
2023-12-04 22:58:56 -0500

    aws setup serverless

---
+++ aws-api-node/index.js
@@ -0,0 +1,13 @@
+export async function handler(event) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify(
+      {
+        message: "Hello",
+        input: event,
+      },
+      null,
+      2
+    ),
+  };
+}
