825eac1b2591ca3e75f4b4efa2cb7342c243b8a9
Carlos Diaz <carlos9559@gmail.com>
2023-12-05 22:27:48 -0500

    Create, Read and Update Ads with dynamodb

--- aws-api-node/src/createAd.js
+++ aws-api-node/src/createAd.js
@@ -2,28 +2,33 @@ const { v4 } = require("uuid");
 const AWS = require("aws-sdk");
 
 const createAd = async (event) => {
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
-  const { title, description } = JSON.parse(event.body);
-  const createdAt = new Date();
-  const id = v4();
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const { title, description } = JSON.parse(event.body);
+    const createdAt = new Date();
+    const id = v4();
 
-  const newAd = {
-    id,
-    title,
-    description,
-    createdAt,
-  };
-  await dynamodb
-    .put({
-      TableName: "AdsTable",
-      Item: newAd,
-    })
-    .promise();
+    const newAd = {
+      id,
+      title,
+      description,
+      createdAt,
+      visible: true,
+    };
+    await dynamodb
+      .put({
+        TableName: "AdsTable",
+        Item: newAd,
+      })
+      .promise();
 
-  return {
-    statusCode: 200,
-    body: JSON.stringify(newAd),
-  };
+    return {
+      status: 200,
+      body: newAd,
+    };
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 module.exports = {
