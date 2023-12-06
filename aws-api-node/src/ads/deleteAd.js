acc7078d1ede313fcd897b468204dc670a2d9b77
Carlos Diaz <carlos9559@gmail.com>
2023-12-06 11:18:12 -0500

    crud with ads

---
+++ aws-api-node/src/ads/deleteAd.js
@@ -0,0 +1,29 @@
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const deleteAd = async (event) => {
+  try {
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const { id } = event.pathParameters;
+
+    await dynamodb
+      .delete({
+        TableName: "AdsTable",
+        Key: { id },
+      })
+      .promise();
+
+    return {
+      status: 200,
+      body: {
+        message: "Ad deleted"
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  deleteAd,
+};
