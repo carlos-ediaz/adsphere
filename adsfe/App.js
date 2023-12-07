b7003a118ff1951a0b3a4fd921079c91d2230c7f
Carlos Diaz <carlos9559@gmail.com>
2023-12-06 23:41:14 -0500

    Stack routes and first components in UI

--- adsfe/App.js
+++ adsfe/App.js
@@ -1,20 +1,33 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { StyleSheet, Text, View } from "react-native";
+import Welcome from "./screens/Welcome";
+import SignIn from "./screens/SignIn";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Working!!</Text>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Group>
+          <Stack.Screen name="Sign In" component={SignIn} />
+        </Stack.Group>
+        <Stack.Group>
+          <Stack.Screen name="Welcome" component={Welcome} />
+        </Stack.Group>
+      </Stack.Navigator>
       <StatusBar style="auto" />
-    </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
